2955781abbf96542678fbdf5c63d640bc30f94f0
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-17 04:55:33 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,3 @@
 def first_challenge
-   epic_tragedy = { :montague => { :patriarch => {}, :matriach => {}, :hero => {}, :hero_friends => {} }, :capulet => { :patriarch => {}, :matriarch => {}, :heroine :=> {}, :heroine_friends{} } }
+   epic_tragedy = { :montague => { :patriarch => { :name => {"Lord Montague"} }, :matriach => { :age => {"Lady Montague"}}, :hero => {}, :hero_friends => {} }, :capulet => { :patriarch => {}, :matriarch => {}, :heroine :=> {}, :heroine_friends{} } }
 end
